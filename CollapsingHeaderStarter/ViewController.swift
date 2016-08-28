2e43c3c0f5ed929ff50c9bb65d31b3f465f3dbd1
emilgras <emil.gras@gmail.com>
2016-08-28 14:59:27 +0200

    Initial Commit

---
+++ CollapsingHeaderStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollapsingHeaderStarter
+//
+//  Created by Emil Gräs on 28/08/2016.
+//  Copyright © 2016 Emil Gräs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
